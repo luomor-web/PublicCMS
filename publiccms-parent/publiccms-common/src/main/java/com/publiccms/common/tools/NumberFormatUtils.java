2bee5aba4030aacef6020fe3bd1e6365d7cc4c30
sanluan <master@publiccms.com>
2025-01-20 11:01:47 +0800

    bug fixed:js error

---
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/tools/NumberFormatUtils.java
@@ -0,0 +1,40 @@
+package com.publiccms.common.tools;
+
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * DateFormatUtil
+ *
+ */
+public class NumberFormatUtils {
+	private NumberFormatUtils() {
+	}
+
+	private static ThreadLocal<Map<String, DecimalFormat>> threadLocal = new ThreadLocal<>();
+
+	/**
+	 * short date format
+	 */
+	public static final String NORMAL_FORMAT_STRING = "0";
+
+	/**
+	 * @param pattern
+	 * @return number format
+	 */
+	public static DecimalFormat getFormat(String pattern) {
+		Map<String, DecimalFormat> map = threadLocal.get();
+		DecimalFormat format = null;
+		if (null == map) {
+			map = new HashMap<>();
+			format = new DecimalFormat(pattern);
+			map.put(pattern, format);
+			threadLocal.set(map);
+		} else {
+			format = map.computeIfAbsent(pattern, DecimalFormat::new);
+		}
+		return format;
+	}
+}
