ad5763683fbdfc1709d423aa0da56b293928df9c
sanluan <master@publiccms.com>
2025-06-18 09:33:33 +0800

    update framework

--- publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/datasource/IDSequenceStyleGenerator.java
+++ publiccms-parent/publiccms-common/src/main/java/com/publiccms/common/datasource/IDSequenceStyleGenerator.java
@@ -5,7 +5,6 @@ import java.util.Properties;
 import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.relational.QualifiedName;
 import org.hibernate.boot.model.relational.QualifiedNameParser;
-import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 import org.hibernate.id.enhanced.SequenceStyleGenerator;
 import org.hibernate.internal.util.config.ConfigurationHelper;
@@ -33,16 +32,15 @@ public class IDSequenceStyleGenerator extends SequenceStyleGenerator {
     public static final String DEF_SEQUENCE_SUFFIX = "_ID_SEQ";
 
     @Override
-    protected QualifiedName determineSequenceName(Properties params, Dialect dialect, JdbcEnvironment jdbcEnv,
-            ServiceRegistry serviceRegistry) {
+    protected QualifiedName determineSequenceName(Properties params, JdbcEnvironment jdbcEnv, ServiceRegistry serviceRegistry) {
         String tableName = ConfigurationHelper.getString(CONFIG_TARGET_TABLE, params);
         if (null == tableName) {
-            return super.determineSequenceName(params, dialect, jdbcEnv, serviceRegistry);
+            return super.determineSequenceName(params, jdbcEnv, serviceRegistry);
         } else {
             Identifier catalog = jdbcEnv.getIdentifierHelper().toIdentifier(ConfigurationHelper.getString(CATALOG, params));
             Identifier schema = jdbcEnv.getIdentifierHelper().toIdentifier(ConfigurationHelper.getString(SCHEMA, params));
-            return new QualifiedNameParser.NameParts(catalog, schema,
-                    jdbcEnv.getIdentifierHelper().toIdentifier(CommonUtils.joinString(tableName.toUpperCase(), DEF_SEQUENCE_SUFFIX)));
+            return new QualifiedNameParser.NameParts(catalog, schema, jdbcEnv.getIdentifierHelper()
+                    .toIdentifier(CommonUtils.joinString(tableName.toUpperCase(), DEF_SEQUENCE_SUFFIX)));
         }
     }
 
